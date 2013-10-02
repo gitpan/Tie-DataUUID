26a612292cd0b5734450d3691f47c0412c29da9c
Mark Fowler <mark@twoshortplanks.com>
2013-10-02 22:32:52 +0000

    Import of MARKF/Tie-DataUUID-1.02 from CPAN.
    gitpan-cpan-distribution: Tie-DataUUID
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         MARKF/Tie-DataUUID-1.02.tar.gz
    gitpan-cpan-author:       MARKF
    gitpan-cpan-maturity:     released

--- lib/Tie/DataUUID.pm
+++ lib/Tie/DataUUID.pm
@@ -1,13 +1,12 @@
 package Tie::DataUUID;
 
 use strict;
-use vars qw($VERSION $uuid @ISA);
-#use warnings;
-
-$VERSION = "1.00";
+use vars qw($VERSION @ISA);
 
 use Tie::Scalar;
 @ISA = qw(Tie::StdScalar);
+
+$VERSION = "1.02";
 
 use Data::UUID;
 
@@ -45,18 +44,24 @@ my $datauuid = Data::UUID->new();
 sub import
 {
   my $class = shift;
-  my $args = shift;
 
-  if (defined $args && $args eq '$uuid')
+  foreach my $args (@_) {
   {
+    unless (defined $args && $args eq '$'.'uuid') {
+      die qq{"$args" is not exported by the }.__PACKAGE__.qq{ module\n}
+    }
+
     my $uuid;
     tie $uuid, $class;
     no strict 'refs';   # about to export symbols
     *{ caller() . "::uuid" } = \$uuid;
+    }
   }
+
+  return
 }
 
-sub FETCH { $datauuid->create_str }
+sub FETCH { return $datauuid->create_str }
 
 =head1 AUTHOR
 
@@ -64,15 +69,20 @@ Written by Mark Fowler E<lt>mark@twoshortplanks.comE<gt>
 
 Copyright Fotango 2004.  All Rights Reserved.
 
+Copyright Mark Fowler 2009, 2013.  All Rights Reserved.
+
 This program is free software; you can redistribute it
 and/or modify it under the same terms as Perl itself.
 
 =head1 BUGS
 
-None known.
+Bugs (or feature requests) should be reported via this distribution's
+CPAN RT queue.  This can be found at
+L<https://rt.cpan.org//Dist/Display.html?Queue=Tie-DataUUID>
 
-Bugs should be reported to me via the CPAN RT system.
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tie::DataUUID>.
+You can also address issues by forking this distribution
+on github and sending pull requests.  It can be found at
+L<http://github.com/2shortplanks/Tie-DataUUID>
 
 =head1 SEE ALSO
 
